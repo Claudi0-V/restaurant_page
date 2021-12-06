90ff1be3788499d0288111f01d140ee7c4d3be8a
Claudi0-V <claudiov.dejesus@protonmail.com>
2021-12-05 21:01:54 -0300

    clean code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import "./styles/index.css"
-import createHeader  from "./modules/header.js"
-import createNavBar  from "./modules/navbar.js"
-import createMain  from "./modules/main.js"
-import createMenu  from "./modules/menu.js"
-import createContact  from "./modules/contact.js"
+import createHeader from "./modules/header.js"
+import createNavBar from "./modules/navbar.js"
+import createMain from "./modules/main.js"
+import createMenu from "./modules/menu.js"
+import createContact from "./modules/contact.js"
+
 
 createHeader()
 createNavBar()
@@ -15,12 +16,12 @@ const tabs = document.querySelectorAll('[data-targetId]');
 const tabContent = document.querySelectorAll('.tab-section');
 
 tabs.forEach(tab => {
-	tab.addEventListener('click', () => {
-	const target = document.querySelector(tab.dataset.targetid);
-	tabContent.forEach(tab => tab.classList.remove('active'));
-	tabs.forEach(tab => tab.classList.remove('active'));
-	tab.classList.add('active');
-	target.classList.add('active');
-	})
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.targetid);
+        tabContent.forEach(tab => tab.classList.remove('active'));
+        tabs.forEach(tab => tab.classList.remove('active'));
+        tab.classList.add('active');
+        target.classList.add('active');
+    })
 })
 
